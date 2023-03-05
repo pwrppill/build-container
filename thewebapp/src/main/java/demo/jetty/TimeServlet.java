5a04596fd5fb72849a90d6270b317c1c744d7b2e
pwrppill <123949615+pwrppill@users.noreply.github.com>
2023-03-05 13:22:27 +0300

    Add files via upload

---
+++ thewebapp/src/main/java/demo/jetty/TimeServlet.java
@@ -0,0 +1,48 @@
+//
+//  ========================================================================
+//  Copyright (c) Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package demo.jetty;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/time/")
+public class TimeServlet extends HttpServlet
+{
+    private static final TimeZone TZ = TimeZone.getDefault();
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        Locale locale = req.getLocale();
+        Calendar cal = Calendar.getInstance(TZ,locale);
+        String dateStr = DateFormat.getDateInstance(DateFormat.DEFAULT,locale).format(cal.getTime());
+        String timeStr = DateFormat.getTimeInstance(DateFormat.DEFAULT,locale).format(cal.getTime());
+        String tzStr = TZ.getDisplayName(false,TimeZone.SHORT,locale);
+        resp.getWriter().println(String.format("%s %s %s",dateStr,timeStr,tzStr));
+    }
+}
